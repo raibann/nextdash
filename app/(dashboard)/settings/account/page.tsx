9bf01e688648040df85f5fce9f16aeda467e59c4
@bann007 <raibann.rb@gmail.com>
2025-11-26 17:29:38 +0700

    add sample admin

---
+++ app/(dashboard)/settings/account/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { ContentSection } from '../_components/content-section'
+import { AccountForm } from './account-form'
+
+export default function SettingsAccount() {
+  return (
+    <ContentSection
+      title='Account'
+      desc='Update your account settings. Set your preferred language and
+          timezone.'
+    >
+      <AccountForm />
+    </ContentSection>
+  )
+}
---
+++ app/(dashboard)/users/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { ConfigDrawer } from '@/components/config-drawer'
+import { Header } from '@/components/layout/header'
+import { Main } from '@/components/layout/main'
+import { ProfileDropdown } from '@/components/profile-dropdown'
+import { Search } from '@/components/search'
+import { ThemeSwitch } from '@/components/theme-switch'
+import { UsersDialogs } from './_components/users-dialogs'
+import { UsersPrimaryButtons } from './_components/users-primary-buttons'
+import { UsersProvider } from './_components/users-provider'
+import { UsersTable } from './_components/users-table'
+import { users } from './_data/users'
+
+export default function Users() {
+  return (
+    <UsersProvider>
+      <Header fixed>
+        <Search />
+        <div className='ms-auto flex items-center space-x-4'>
+          <ThemeSwitch />
+          <ConfigDrawer />
+          <ProfileDropdown />
+        </div>
+      </Header>
+
+      <Main className='flex flex-1 flex-col gap-4 sm:gap-6'>
+        <div className='flex flex-wrap items-end justify-between gap-2'>
+          <div>
+            <h2 className='text-2xl font-bold tracking-tight'>User List</h2>
+            <p className='text-muted-foreground'>
+              Manage your users and their roles here.
+            </p>
+          </div>
+          <UsersPrimaryButtons />
+        </div>
+        <UsersTable data={users} search={{}} navigate={() => {}} />
+      </Main>
+
+      <UsersDialogs />
+    </UsersProvider>
+  )
+}
